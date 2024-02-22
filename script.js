68f3ed4c89c056020b6635ae2fac921e4e4bfd38
24578067 <antyusindaniil@gmail.com>
2024-02-22 16:38:30 +0300

    Center
    Center

---
+++ script.js
@@ -0,0 +1,78 @@
+const images = document.querySelectorAll('.slider-img');
+const controlls = document.querySelectorAll('.controlls');
+let imageIndex = 0;
+
+function show(index) {
+    images[imageIndex].classList.remove('active');
+    images[index].classList.add('active');
+    imageIndex = index;
+}
+
+controlls.forEach((e) => {
+    e.addEventListener('click', (event) => {
+        if (event.target.classList.contains('prev')) {
+            let index = imageIndex - 1;
+
+            if (index < 0) {
+                index = images.length - 1;
+            }
+
+            show(index);
+        } else if (event.target.classList.contains('next')) {
+            let index = imageIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            show(index);
+        }
+    })
+})
+
+show(imageIndex);
+
+
+
+
+const animItems = document.querySelectorAll('._anim-items')
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+            
+            
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart
+            }
+
+            if ((window.pageYOffset > animItemOffset - animItemPoint) && window.pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_act')
+            } else {
+                if(!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_act')
+                }
+                
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return {
+                top: rect.top + scrollTop, 
+                left: rect.left + scrollLeft
+            }
+    }
+
+    setTimeout(() => {
+        animOnScroll()
+    }, 100)
+    
+}
